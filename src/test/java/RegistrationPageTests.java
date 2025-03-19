fe8f4a92063b6879e1bf97b75ac50cfa5f863c44
mimmato <mimmato@gmail.com>
2025-03-19 18:48:25 +0200

    saving progress on username field validation on reg page

--- src/test/java/RegistrationPageTests.java
+++ src/test/java/RegistrationPageTests.java
@@ -9,51 +9,13 @@ public class RegistrationPageTests extends BaseTestConfig {
 
     private static final Logger log = LoggerFactory.getLogger(RegistrationPageTests.class);
 
-//    @Test
-//    public void navToRegistrationPage() {
-//        WebDriver driver = getDriver();
-//
-//        BasePageObject basePage = new BasePageObject(driver);
-//
-//        String uri = "/posts/all";
-//        basePage.openURL(uri);
-//        boolean isCorrectURL = basePage.isCurrentURLCorrect(uri);
-//        Assert.assertTrue(isCorrectURL, "The URL did not match the expected Landing page.");
-//        basePage.clickLoginLink();
-//
-//        LoginPageObject loginPage = new LoginPageObject(driver);
-//
-//        uri = "/users/login";
-//        isCorrectURL = loginPage.isCurrentURLCorrect(uri);
-//        Assert.assertTrue(isCorrectURL, "The URL did not match the expected Login page: " + uri);
-//
-//        loginPage.isRegisterLinkVisible();
-//
-//        String registerText = loginPage.isRegisterLinkVisible();
-//        Assert.assertEquals(registerText, "Register", "The Register link text does not match the expected: " + registerText);
-//        loginPage.clickRegister();
-//
-//        RegistrationPageObject registrationPage = new RegistrationPageObject(driver);
-//
-//        uri = "/users/register";
-//        isCorrectURL = registrationPage.isCurrentURLCorrect(uri);
-//        Assert.assertTrue(isCorrectURL, "The URL did not match the expected Registration page.");
-//
-//        registrationPage.validateFieldsText();
-//
-//        //user length should be between 2 and 20 characters as per the page invalid-feedback
-//        //<span _ngcontent-lgj-c4="" class="invalid-feedback ng-star-inserted"> Maximum 20 characters! </span>
-//        //<span _ngcontent-lgj-c4="" class="invalid-feedback ng-star-inserted"> Minimum 2 characters ! </span>
-//    }
-
-
     @Test
     public void testValidUsername() {
         WebDriver driver = getDriver();
         RegistrationPageObject registrationPage = new RegistrationPageObject(driver);
 
         BasePageObject basePage = new BasePageObject(driver);
-        basePage.navigateToRegistrationPage(); // This takes care of the entire navigation
+        basePage.navigateToRegistrationPage();
 
         // Now proceed with the username validation
         registrationPage.validateFieldsText();
@@ -69,9 +31,8 @@ public class RegistrationPageTests extends BaseTestConfig {
         RegistrationPageObject registrationPage = new RegistrationPageObject(driver);
 
         BasePageObject basePage = new BasePageObject(driver);
-        basePage.navigateToRegistrationPage(); // This takes care of the entire navigation
+        basePage.navigateToRegistrationPage();
 
-        // Now proceed with the username validation
         registrationPage.validateFieldsText();
 
         String validUser = registrationPage.genRandomUser(27);
@@ -86,9 +47,9 @@ public class RegistrationPageTests extends BaseTestConfig {
         RegistrationPageObject registrationPage = new RegistrationPageObject(driver);
 
         BasePageObject basePage = new BasePageObject(driver);
-        basePage.navigateToRegistrationPage(); // This takes care of the entire navigation
+        basePage.navigateToRegistrationPage();
 
-        // Now proceed with the username validation
+
         registrationPage.validateFieldsText();
 
         String validUser = registrationPage.genRandomUser(2);
